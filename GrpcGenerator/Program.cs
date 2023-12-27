ee296475b8bf9678ef13208c059255b91e173003
MarkoTunjic <marko.tunjic21@gmail.com>
2023-12-27 16:19:42 +0100

    add configuration reader

--- GrpcGenerator/Program.cs
+++ GrpcGenerator/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
+IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
