79f8564afbfcf3bf4ef615273a937f4af960bbe1
MarkoTunjic <marko.tunjic21@gmail.com>
2023-12-30 21:55:40 +0100

    create Mapper generator structure

---
+++ GrpcGenerator/Generators/MapperGenerators/Impl/DotnetMapperGenerator.cs
@@ -0,0 +1,9 @@
+namespace GrpcGenerator.Generators.MapperGenerators.Impl;
+
+public class DotnetMapperGenerator : IMapperGenerator
+{
+    public void GenerateMappers(string pathToModels, string targetDirectory)
+    {
+        throw new NotImplementedException();
+    }
+}
