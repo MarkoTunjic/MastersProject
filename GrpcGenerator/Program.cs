b7350caff140fbfa1a766d80f75b96de74e6bb3b
MarkoTunjic <marko.tunjic21@gmail.com>
2024-04-28 00:00:06 +0200

    Wrap as grpc project

--- GrpcGenerator/Program.cs
+++ GrpcGenerator/Program.cs
@@ -1,93 +1,19 @@
-﻿using GrpcGenerator.Domain;
-using GrpcGenerator.Generators.AdditionalActions;
-using GrpcGenerator.Generators.AdditionalActions.Impl;
-using GrpcGenerator.Generators.ConfigGenerators;
-using GrpcGenerator.Generators.ConfigGenerators.Impl;
-using GrpcGenerator.Generators.DependencyGenerators;
-using GrpcGenerator.Generators.DependencyGenerators.Impl;
-using GrpcGenerator.Generators.DtoGenerators;
-using GrpcGenerator.Generators.DtoGenerators.Impl;
-using GrpcGenerator.Generators.MapperGenerators;
-using GrpcGenerator.Generators.MapperGenerators.Impl;
-using GrpcGenerator.Generators.ModelGenerators;
-using GrpcGenerator.Generators.ModelGenerators.Impl;
-using GrpcGenerator.Generators.PresentationGenerators;
-using GrpcGenerator.Generators.PresentationGenerators.Impl.DotNet;
-using GrpcGenerator.Generators.PresentationGenerators.Impl.Rest.DotNet;
-using GrpcGenerator.Generators.RepositoryGenerators;
-using GrpcGenerator.Generators.RepositoryGenerators.Impl;
-using GrpcGenerator.Generators.ServiceGenerators;
-using GrpcGenerator.Generators.ServiceGenerators.Impl;
-using GrpcGenerator.Utils;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using GrpcGenerator.Services;
 
-var file = new FileInfo("../../../appsettings.json");
-IConfiguration config = new ConfigurationBuilder()
-    .AddJsonFile(file.DirectoryName + "/" + file.Name)
-    .Build();
+var builder = WebApplication.CreateBuilder(args);
 
-var services = new ServiceCollection();
-services.AddSingleton(config);
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
-var guid = Guid.NewGuid().ToString();
+// Add services to the container.
+builder.Services.AddGrpc();
 
-const string oldSolutionName = "Template";
-const string oldProjectName = "Template";
+var app = builder.Build();
 
-Copier.CopyDirectory($"{config["sourceCodeRoot"]}/templates/dotnet6/{oldSolutionName}",
-    $"{config["sourceCodeRoot"]}/{guid}/{oldSolutionName}");
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-const string newSolutionName = "FirstSolution";
-const string newProjectName = "FirstProject";
-var databaseName = "MastersProject";
-var databaseServer = "127.0.0.1";
-var databasePort = "1433";
-var databaseUid = "sa";
-var databasePwd = "Drvahagn10.";
-var provider = "sqlserver";
-var projectRoot = $"{config["sourceCodeRoot"]}/{guid}/{newSolutionName}/{newProjectName}";
-var architecture = "grpc";
-var generatorVariables =
-    new GeneratorVariables(
-        new DatabaseConnection(databaseServer, databaseName, databasePort, databasePwd, databaseUid, provider),
-        newProjectName, newSolutionName, projectRoot, provider, architecture);
-GeneratorVariablesProvider.AddVariables(guid, generatorVariables);
-
-ProjectRenamer.RenameDotNetProject($"{config["sourceCodeRoot"]}/{guid}", oldSolutionName, oldProjectName,
-    guid);
-
-ServicesProvider.SetServices(services.BuildServiceProvider());
-
-IDependencyGenerator dependencyGenerator = new DotNetDependencyGenerator();
-dependencyGenerator.GenerateDependencies(guid,
-    $"{config["sourceCodeRoot"]}/{guid}/{newSolutionName}/{newProjectName}/{newProjectName}.csproj");
-
-IConfigGenerator databaseConfigGenerator = new DotNetDatabaseConfigGenerator();
-databaseConfigGenerator.GenerateConfig($"{config["sourceCodeRoot"]}/{guid}/{newSolutionName}/{newProjectName}", guid);
-
-IModelGenerator modelGenerator = new EfCoreModelGenerator();
-modelGenerator.GenerateModels(guid);
-
-IDtoGenerator dtoGenerator = new DotNetDtoGenerator();
-dtoGenerator.GenerateDtos(guid);
-
-IMapperGenerator mapperGenerator = new DotnetMapperGenerator();
-mapperGenerator.GenerateMappers(guid);
-
-IAdditionalAction registerServices = new RegisterServicesAdditionalAction();
-registerServices.DoAdditionalAction(guid);
-
-IRepositoryGenerator repositoryGenerator = new DotNetRepositoryGenerator();
-repositoryGenerator.GenerateRepositories(guid);
-
-IServiceGenerator serviceGenerator = new DotNetServiceGenerator();
-serviceGenerator.GenerateServices(guid);
-
-IPresentationGenerator presentationGenerator = architecture == "grpc" ? new DotNetGrpcPresentationGenerator() : new DotnetRestGenerator();
-presentationGenerator.GeneratePresentation(guid);
-
-Zipper.ZipDirectory($"{config["sourceCodeRoot"]}/{guid}",
-    $"{config["sourceCodeRoot"]}/{config["mainProjectName"]}/FirstSolution.zip");
-
-Directory.Delete($"{config["sourceCodeRoot"]}/{guid}", true);
+app.Run();
